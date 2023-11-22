390841bcd2f6112b8e0fd6ed3045f9194abf616d
... <37700859+453594848@users.noreply.github.com>
2023-11-22 20:14:08 +0800

    添加公共字段

---
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -0,0 +1,21 @@
+package com.sky.annotation;
+/*
+* 自定义注解，标识字段自动填充
+* */
+
+import com.sky.enumeration.OperationType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//注解在代码运行时的生命周期
+@Retention(RetentionPolicy.RUNTIME)
+//注解的作用目标
+@Target(ElementType.METHOD)
+
+public @interface AutoFill {
+    //数据库操作类型
+    OperationType value();
+}
---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,82 @@
+package com.sky.aspect;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+//切面类
+@Aspect
+
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /*
+     * 切入点
+     * */
+    @Pointcut("@annotation(com.sky.annotation.AutoFill)")
+    private void autoFillPointCut() {
+    }
+
+    /*
+     * 连接点
+     * */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint) {
+        log.info("开启自动填充");
+        //获取被拦截方法的操作类型
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();//获取方法签名对象
+        OperationType value = signature
+                .getMethod()
+                .getAnnotation(AutoFill.class)//获取方法的注解对象
+                .value();//获取注解的操作类型
+        //获取被拦截方法的参数
+        Object[] args = joinPoint.getArgs();
+        if (args == null || args.length == 0) {
+            return;
+        }
+
+        //进行的操作
+        if (value == OperationType.INSERT) {
+            try {
+                //为四个公共字段赋值
+                args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class)
+                        .invoke(args[0], LocalDateTime.now());
+                args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class)
+                        .invoke(args[0], LocalDateTime.now());//通过反射进行赋值
+
+                Method setUpdateUser = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+                Method setCreatUser = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                setCreatUser.invoke(args[0], BaseContext.getCurrentId());
+                setUpdateUser.invoke(args[0], BaseContext.getCurrentId());
+
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (value == OperationType.UPDATE) {
+            try {
+                args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class)
+                        .invoke(args[0], LocalDateTime.now());
+                Method setUpdateUser = args[0].getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+                setUpdateUser.invoke(args[0], BaseContext.getCurrentId());
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
