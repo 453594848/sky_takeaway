ac3260992820cf739f233c33357c5e02fbe1796f
... <37700859+453594848@users.noreply.github.com>
2023-11-29 01:39:07 +0800

    完善服务端订单功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -54,4 +54,11 @@ public interface OrderMapper {
 * */
     @Delete("delete from sky_take_out.orders where id=#{id}")
     void deleteByOrderId(Long id);
+
+    /**
+     * 根据状态统计订单数量
+     * @param status
+     */
+    @Select("select count(id) from sky_take_out.orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
